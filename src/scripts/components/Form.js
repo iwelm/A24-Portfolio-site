4b8ab3a1143e57d0c4eee40c750d6143551d4e53
iwelm <wilem.lavoie@gmail.com>
2024-10-02 00:37:56 -0400

    form fonctionnel!!!!

--- src/scripts/components/Form.js
+++ src/scripts/components/Form.js
@@ -1,73 +1,34 @@
 export default class Form {
   constructor(element) {
-    this.element = element; // Correspont a notre data-component"Form"
-    this.formElements = this.element.elements;
+    this.element = element; // Correspond à notre data-component="Form"
     this.init();
   }
 
   init() {
-    this.element.setAttribute('novalidate', ''); //empeche la notifications validation d'afficher donc le formualire sera envoyé meme si des champs requis sont vides.
-
-    for (let i = 0; i < this.formElements.length; i++) {
-      const input = this.formElements[i];
-
-      if (input.required) {
-        input.addEventListener('input', this.validateInput.bind(this));
-      }
-    }
-
+    this.element.setAttribute('novalidate', ''); // Empêche la notification de validation d'afficher
     this.element.addEventListener('submit', this.onSubmit.bind(this));
   }
 
   onSubmit(event) {
-    event.preventDefault();
+    event.preventDefault(); // Empêche le comportement par défaut
 
-    if (this.validate()) {
-      console.log('success');
-      //envoi ajax du formulaire
-      this.showConfirmation();
-    } else {
-      console.log('fail');
-    }
-  }
-
-  validate() {
-    let isValid = true;
-    for (let i = 0; i < this.formElements.length; i++) {
-      const input = this.formElements[i];
-
-      if (input.required && !this.validateInput(input)) {
-        isValid = false;
-      }
-    }
-
-    return isValid;
-  }
-
-  validateInput(event) {
-    const input = event.currentTarget || event;
-    if (input.validity.valid) {
-      this.removeError(input);
-    } else {
-      this.addError(input);
-    }
-
-    return input.validity.valid;
-  }
-
-  addError(input) {
-    const container =
-      input.closest('[data-input-container]') || input.closest('.input');
-    container.classList.add('error');
-  }
-
-  removeError(input) {
-    const container =
-      input.closest('[data-input-container]') || input.closest('.input');
-    container.classList.remove('error');
+    // Envoi AJAX du formulaire
+    const formData = new FormData(this.element);
+    fetch(this.element.action, {
+      method: 'POST',
+      body: formData,
+    })
+      .then((response) => {
+        if (response.ok) {
+          this.showConfirmation(); // Appelle la confirmation si l'envoi réussit
+        } else {
+          console.error("Erreur lors de l'envoi du formulaire");
+        }
+      })
+      .catch((error) => console.error('Erreur:', error));
   }
 
   showConfirmation() {
-    this.element.classList.add('is-sent');
+    this.element.classList.add('is-sent'); // Indique que le formulaire a été envoyé
   }
 }
