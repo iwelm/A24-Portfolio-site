4900041ab4eca948348ec4083591be604f6eb7c6
iwelm <wilem.lavoie@gmail.com>
2024-10-19 13:42:46 -0400

    Ajout de la page 404

---
+++ src/theme/404.php
@@ -0,0 +1,14 @@
+<?php get_header(); ?>
+
+<section class="formulaire">
+    <div class="wrapper">
+            <div class="form_confirmation">
+                <h2>404</h2>
+                <p>Oh non la page que vous recherchez est introuvable. Il est temps de <span class="orange">retourner à l'acceuil.</span></p>
+            </div>
+        </form>
+    </div>
+</section>
+
+
+<?php get_footer(); ?>
