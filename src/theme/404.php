cbdfbff4a18702bd9f8d6442a1e7e9dc478bbb5b
iwelm <wilem.lavoie@gmail.com>
2024-11-02 15:16:07 -0400

    Ajout d'un bouton accueil dans 404

--- src/theme/404.php
+++ src/theme/404.php
@@ -4,7 +4,9 @@
     <div class="wrapper">
             <div class="form_confirmation">
                 <h2>404</h2>
-                <p>Oh non, la page que vous recherchez est introuvable. Il est temps de retourner à l'accueil.</p>
+                <p>Oh non, la page que vous recherchez est introuvable.</p>
+                <a class="bouton" href="https://wlavoie.ca/">retour à l'accueil</a>
+                <a class="bouton-responsive" href="https://wlavoie.ca/">retour à l'accueil</a>
             </div>
         </form>
     </div>
